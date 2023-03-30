4325a2f281bc766d726d1a4458bd031073935d74
rodrigosgomide <rodrigogomide.def@gmail.com>
2023-03-30 17:16:16 -0300

    feat: register user

--- back-end/src/services/UserService.js
+++ back-end/src/services/UserService.js
@@ -1,8 +1,8 @@
 const md5 = require('md5');
+const { Op } = require('sequelize');
 const { User } = require('../database/models');
 const { customError, errorStatus, errorMessages } = require('../utils/errors');
 const { encript } = require('../auth/token');
-// const { passwordCheck } = require('../auth/passwordCript');
 
 const login = async (email, password) => {
   const user = await User.findOne({
@@ -10,12 +10,34 @@ const login = async (email, password) => {
       email,
     },
   });
+
+  if (!user) throw customError(errorStatus.NOT_FOUND, errorMessages.INVALID_FIELDS);
   const isValidPass = md5(password) === user.password;
-  if (!user || !isValidPass) throw customError(errorStatus.NOT_FOUND, errorMessages.INVALID_FIELDS);
+  if (!isValidPass) throw customError(errorStatus.NOT_FOUND, errorMessages.INVALID_FIELDS);
+
   const token = encript({ email: user.email });
   return { token, role: user.role, name: user.name };
 };
 
+const userExist = async (name, email) => {
+  const user = await User.findOne({
+    where: {
+    [Op.or]: [
+      { name },
+      { email },
+    ],
+  },
+  });
+  if (user) throw customError(errorStatus.CONFLICT, errorMessages.USER_ALREDY_EXIST);
+};
+
+const register = async (name, email, password) => {
+  await userExist(name, email);
+  const newUser = await User.create({ name, email, password, role: 'custumer' });
+  const token = encript({ email: newUser.email });
+  return { token, role: newUser.role, name: newUser.name };
+};
 module.exports = {
   login,
+  register,
 };
--- back-end/src/utils/errors.js
+++ back-end/src/utils/errors.js
@@ -3,12 +3,13 @@ const errorStatus = {
     INVALID_FIELDS: 400,
     NOT_FOUND: 404,
     UNAUTHORIZED: 401,
+    CONFLICT: 409,
   };
   
   const errorMessages = {
     IS_REQUIRED: 'Some required fields are missing',
     INVALID_FIELDS: 'Invalid fields',
-    USER_NOT_EXIST: 'User does not exist',
+    USER_ALREDY_EXIST: 'User alredy exist',
     INVALID_CATEGORY: 'one or more "categoryIds" not found',
     POST_NOT_EXIST: 'Post does not exist',
     UNAUTHORIZED_USER: 'Unauthorized user',
