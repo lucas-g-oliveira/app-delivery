dd42570314e184950b93b530f0d32407d7d5aca7
Lucas G <lucasg.devmail93@gmail.com>
2023-04-18 20:51:50 -0300

    fix: restore config db

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,9 +1,22 @@
-const option2 = {
-  host: 'localhost',
-  port: '3306',
-  database: 'delivery-app-dev',
-  username: 'root',
-  password: 'password',
+require('dotenv').config();
+
+const environment = process.env.NODE_ENV || 'test';
+
+const suffix = {
+  prod: '',
+  production: '',
+  dev: '-dev',
+  development: '-dev',
+  test: '-test',
+};
+
+const options = {
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
+  database: 
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
@@ -13,12 +26,12 @@ const option2 = {
 
 module.exports = {
   development: {
-   ...option2,
+    ...options,
   },
   test: {
-    ...option2,
+    ...options,
   },
   production: {
-    ...option2,
+    ...options,
   },
 };
